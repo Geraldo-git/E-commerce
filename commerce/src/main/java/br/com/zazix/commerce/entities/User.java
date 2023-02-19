45644cfda317f4ff8bf16f9920d8955dd5833fa2
Geraldo-git <laboratorio.inicial@gmail.com>
2023-02-19 15:02:17 -0300

    Primeira associação muitos para muitos

--- commerce/src/main/java/br/com/zazix/commerce/entities/User.java
+++ commerce/src/main/java/br/com/zazix/commerce/entities/User.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name ="tb_user")
+@Table(name = "tb_user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String phone;
     private LocalDate birthDate;
