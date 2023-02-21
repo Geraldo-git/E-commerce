ec1c0042cbcf1a653a5b9150b84f877b10cceb6d
Geraldo-git <laboratorio.inicial@gmail.com>
2023-02-21 16:03:08 -0300

    Product POST

--- commerce/src/main/java/br/com/zazix/commerce/services/ProductService.java
+++ commerce/src/main/java/br/com/zazix/commerce/services/ProductService.java
@@ -32,6 +32,21 @@ public class ProductService {
         return list.map(x -> new ProductDTO(x));
     }
 
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+
+        Product entity = new Product();
+
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+
+        return new ProductDTO(entity);
+    }
+
 
 
 }
