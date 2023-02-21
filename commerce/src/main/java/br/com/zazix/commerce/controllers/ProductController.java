d273e580ab85f0d3d4534eaaa4b43d4750c2be15
Geraldo-git <laboratorio.inicial@gmail.com>
2023-02-21 11:43:07 -0300

    Busca paginada

--- commerce/src/main/java/br/com/zazix/commerce/controllers/ProductController.java
+++ commerce/src/main/java/br/com/zazix/commerce/controllers/ProductController.java
@@ -3,10 +3,14 @@ package br.com.zazix.commerce.controllers;
 import br.com.zazix.commerce.DTOs.ProductDTO;
 import br.com.zazix.commerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -15,6 +19,10 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
+    }
 
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
--- commerce/src/main/java/br/com/zazix/commerce/services/ProductService.java
+++ commerce/src/main/java/br/com/zazix/commerce/services/ProductService.java
@@ -4,10 +4,14 @@ import br.com.zazix.commerce.DTOs.ProductDTO;
 import br.com.zazix.commerce.entities.Product;
 import br.com.zazix.commerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -19,6 +23,15 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
-
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> list = repository.findAll(pageable);
+        //return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+        return list.map(x -> new ProductDTO(x));
+    }
+
+
+
 }
