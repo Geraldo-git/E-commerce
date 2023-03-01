cef3171bc89acd53ecfa1a6c37fd6e7327a71fb5
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-01 09:02:22 -0300

    Dados de auditoria

--- commerce/src/main/java/br/com/zazix/commerce/entities/Category.java
+++ commerce/src/main/java/br/com/zazix/commerce/entities/Category.java
@@ -3,6 +3,7 @@ package br.com.zazix.commerce.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -15,6 +16,11 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
@@ -40,6 +46,23 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     public Set<Product> getProducts() {
