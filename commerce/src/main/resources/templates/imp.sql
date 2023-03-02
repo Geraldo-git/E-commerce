06cb6a73f3de7d9a8014c363e30dfe4a2c1e9eed
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-02 08:22:38 -0300

    Ajuste POM versao 3.0.2

---
+++ commerce/src/main/resources/templates/imp.sql
@@ -0,0 +1,4 @@
+insert INTO tb_category(name) VALUES ('Books');
+insert INTO tb_category(name) VALUES ('Garden');
+insert INTO tb_category(name) VALUES ('Computers');
+insert INTO tb_category(name) VALUES ('Kitchens');
