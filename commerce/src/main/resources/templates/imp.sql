1aa985eddb8ea44607bb283f667d1df9a6f38d71
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-07 12:08:28 -0300

    ajustes finais

--- commerce/src/main/resources/templates/imp.sql
+++
@@ -1,4 +0,0 @@
-insert INTO tb_category(name) VALUES ('Books');
-insert INTO tb_category(name) VALUES ('Garden');
-insert INTO tb_category(name) VALUES ('Computers');
-insert INTO tb_category(name) VALUES ('Kitchens');
