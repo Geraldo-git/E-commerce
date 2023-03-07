78a784f71c31212a703b75f4b025ec998cbafe3e
Geraldo-git <laboratorio.inicial@gmail.com>
2023-03-07 08:36:45 -0300

    Autenticacao em memoria

--- commerce/src/main/java/br/com/zazix/commerce/CommerceApplication.java
+++ commerce/src/main/java/br/com/zazix/commerce/CommerceApplication.java
@@ -2,6 +2,9 @@ package br.com.zazix.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CommerceApplication {
@@ -10,5 +13,7 @@ public class CommerceApplication {
 
 		SpringApplication.run(CommerceApplication.class, args);
 	}
+
+
 
 }
--- commerce/src/main/java/br/com/zazix/commerce/config/security/WebSecurityConfig.java
+++ commerce/src/main/java/br/com/zazix/commerce/config/security/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package br.com.zazix.commerce.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,5 +21,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("master")
+                .password(passwordEncoder().encode("1010"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
